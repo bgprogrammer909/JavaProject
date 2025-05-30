954af32605f156b047ae9a4abcd10aeed513246e
suchit <suchitbajracharya1@gmail.com>
2025-05-30 18:04:16 +0545

    verify

---
+++ src/clinicandpharmacymanagement/controller/PatientDashboardController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clinicandpharmacymanagement.controller;
+
+import clinicandpharmacymanagement.view.PatientDashboard;
+import clinicandpharmacymanagement.view.model.UserData;
+
+
+
+/**
+ *
+ * @author ACER
+ */
+public class PatientDashboardController {
+    PatientDashboard view;
+    UserData user;
+    public PatientDashboardController(PatientDashboard view,UserData user){
+        this.view=view;
+        this.user=user;
+    }
+    public void open(){
+        view.setVisible(true);
+    }
+    public void close(){
+        view.dispose();
+    }
+    
+    
+}
