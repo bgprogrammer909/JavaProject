7524cb047327a64944c0482770ff211cea5cd648
suchit <suchitbajracharya1@gmail.com>
2025-06-08 17:46:05 +0545

    change

--- src/clinicandpharmacymanagement/controller/AdminController.java
+++ src/clinicandpharmacymanagement/controller/AdminController.java
@@ -1,30 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package clinicandpharmacymanagement.controller;
 
+import clinicandpharmacymanagement.Dao.UserDao;
 import clinicandpharmacymanagement.view.AdminDashboardPharma;
-import clinicandpharmacymanagement.view.AdminDashboardPharma1;
 import clinicandpharmacymanagement.view.model.UserData;
 
-/**
- *
- * @author ACER
- */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+
 public class AdminController {
     AdminDashboardPharma view;
     UserData user;
-    public AdminController(  AdminDashboardPharma view,UserData user){
-        this.view=view;
-        AdminDashboardPharma1 ad2=new AdminDashboardPharma1();
-        new ButtonController(view.getUsers(),this.view,ad2);
-        this.user=user;
+    UserDao userDao = new UserDao();  // Added instance
+
+    public AdminController(AdminDashboardPharma view, UserData user) {
+        this.view = view;
+        this.user = user;
+        // Removed the unnecessary AdminDashboardPharma1 ad2 instantiation
     }
-    public void open(){
+
+    public void open() {
         view.setVisible(true);
     }
-    public void close(){
+
+    public void close() {
         view.dispose();
-}
+    }
+
+    class LoadDataListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            List<UserData> users = userDao.getAllUsers();  // Use instance, not static call
+            DefaultTableModel model = new DefaultTableModel();
+            model.addColumn("ID");
+            model.addColumn("Name");
+            model.addColumn("Email");
+            model.addColumn("Password");
+            model.addColumn("Type");
+
+            for (UserData user : users) {
+                model.addRow(new Object[]{
+                    user.getId(),
+                    user.getName(),
+                    user.getEmail(),
+                    user.getPassword(),
+                    user.getUtype()
+                });
+            }
+
+            view.getTable().setModel(model);
+        }
+    }
 }
