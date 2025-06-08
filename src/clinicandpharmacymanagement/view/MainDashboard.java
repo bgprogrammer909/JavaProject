408012765238c3bf2e48152777cbdfb556897d3c
SAKSHAMBAN219 <sakshamban@gmail.com>
2025-06-08 09:48:26 +0545

    my work

--- src/clinicandpharmacymanagement/view/MainDashboard.java
+++ src/clinicandpharmacymanagement/view/MainDashboard.java
@@ -63,7 +63,7 @@ public class MainDashboard extends javax.swing.JFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(18, 18, 18)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 658, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 688, Short.MAX_VALUE)
                 .addComponent(jButton1)
                 .addContainerGap())
         );
