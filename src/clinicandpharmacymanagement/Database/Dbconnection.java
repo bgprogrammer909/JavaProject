ee82bd495f5032314c7de67dcaffe05583cb0452
suchit <suchitbajracharya1@gmail.com>
2025-06-08 12:48:12 +0545

    last time

--- src/clinicandpharmacymanagement/Database/Dbconnection.java
+++ src/clinicandpharmacymanagement/Database/Dbconnection.java
@@ -3,14 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package clinicandpharmacymanagement.Database;
-
 import java.sql.*;
-
 /**
  *
  * @author ACER
  */
-public interface DbConnection {
+public interface Dbconnection {
     Connection openConnection();
     void closeConnection(Connection conn);
 }
