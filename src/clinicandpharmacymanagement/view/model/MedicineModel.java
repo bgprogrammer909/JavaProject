ec69a34086892c26e090a552ee05a0366e1a73c3
ankit21377 <ankitkharel678@gmail.com>
2025-06-09 13:12:14 +0545

    model error fixed

--- src/clinicandpharmacymanagement/view/model/MedicineModel.java
+++ src/clinicandpharmacymanagement/view/model/MedicineModel.java
@@ -5,7 +5,7 @@
 package clinicandpharmacymanagement.view.model;
 
 import java.util.Date;
-import view.model.MedicineModel;
+
 
 
 /**
@@ -14,55 +14,43 @@ import view.model.MedicineModel;
  */
 public class MedicineModel {
     private String  medicinename;
+
+    public String getMedicinename() {
+        return medicinename;
+    }
+
+    public void setMedicinename(String medicinename) {
+        this.medicinename = medicinename;
+    }
+
+    public Date getExpirydate() {
+        return expirydate;
+    }
+
+    public void setExpirydate(Date expirydate) {
+        this.expirydate = expirydate;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
     private int unit;
     private Date expirydate;
     private String id;
     private String price;
-    
-  public MedicineModel( String medicinename,int unit, Date expiry date, String id, String price) {
+
+    public MedicineModel(String medicinename, int unit, Date expirydate, String id, String price) {
         this.medicinename = medicinename;
         this.unit = unit;
         this.expirydate = expirydate;
         this.id = id;
         this.price = price;
     }
-   public void setmedicinename(String name){
-        this.medicinename=medicinename;
-    }
-    
-     public void setUnit(int unit){
-        this.unit=unit;
-    }
-    
-     public void setexpirydate(Date expirydate){
-        this.expirydate=expirydate;
-    }
      
-    public String getmedicinename(){
-        return this.medicinename;
-    }
-    
-    public int getunit(){
-        return this.unit;
-    }
-    
-    public Date getexpirydate(){
-        return this.expirydate;
-    }
-    
-       public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getId() {
-        return id;
-    }
-    public void setprice(String id){
-        this.price = price;
-    }
-     public String getprice(){
-      return price;   
-    }
      
 }
          
