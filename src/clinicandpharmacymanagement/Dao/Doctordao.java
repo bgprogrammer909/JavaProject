3a7d81947beae476ba29f5ff6940fc27855a1db9
ankit21377 <ankitkharel678@gmail.com>
2025-06-11 12:18:25 +0545

    doctor model

---
+++ src/clinicandpharmacymanagement/Dao/Doctordao.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clinicandpharmacymanagement.Dao;
+
+import clinicandpharmacymanagement.Database.MysqlConnection;
+import clinicandpharmacymanagement.view.model.MedicineModel;
+import java.sql.*;
+/**
+ *
+ * @author AngkitKharel
+ */
+public class Doctordao {
+ MysqlConnection mysql=new MysqlConnection();
+    public boolean medicine(MedicineModel medicinemodel){
+        String query ="insert into medicinemodel(medincename,id,unit,price,expirydate) values(?,?,?,?,?)";
+        Connection conn=mysql.openConnection();
+        try{
+            
+            PreparedStatement stmnt=conn.prepareStatement(query);
+            stmnt.setString(1, medicinemodel.);
+            stmnt.setInt(2, medicinemodel.getunit());
+            stmnt.setDate(3, (Date) medicinemodel.getexpirydate());
+            stmnt.setString(4, medicinemodel.getId());
+            stmnt.setString(5, medicinemodel.getprice());
+            int result=stmnt.executeUpdate();
+            return result>0;
+        } catch(SQLException e){
+            return false;
+        }finally {
+            mysql.closeConnection(conn);
+        }
+ 
+    }
+    
+
--- src/clinicandpharmacymanagement/view/model/DoctorModel.java
+++ src/clinicandpharmacymanagement/view/model/DoctorModel.java
@@ -13,45 +13,70 @@ import java.util.Date;
  * @author AngkitKharel
  */
 public class DoctorModel {
-    private String  medicinename;
-
-    public String getMedicinename() {
-        return medicinename;
-    }
-
-    public void setMedicinename(String medicinename) {
-        this.medicinename = medicinename;
-    }
-
-    public Date getExpirydate() {
-        return expirydate;
-    }
-
-    public void setExpirydate(Date expirydate) {
-        this.expirydate = expirydate;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
-    private int unit;
-    private Date expirydate;
     private String id;
-    private String price;
+    private String name;
+    private String time;
+    private String phone;
 
-    public DoctorModel(String medicinename, int unit, Date expirydate, String id, String price) {
-        this.medicinename = medicinename;
-        this.unit = unit;
-        this.expirydate = expirydate;
-        this.id = id;
-        this.price = price;
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
     }
-     
-     
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the time
+     */
+    public String getTime() {
+        return time;
+    }
+
+    /**
+     * @param time the time to set
+     */
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    
+   
+   
 }
          
      
