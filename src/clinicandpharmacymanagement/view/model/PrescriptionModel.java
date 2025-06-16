3d5c12a0ea552d1b4a99c469e9a1cabb967a61ac
Srizan-mhrz <shrijan.mhrz@gmail.com>
2025-06-16 09:12:46 +0545

    Prescription model done

--- src/clinicandpharmacymanagement/view/model/PrescriptionModel.java
+++ src/clinicandpharmacymanagement/view/model/PrescriptionModel.java
@@ -8,18 +8,50 @@ package clinicandpharmacymanagement.view.model;
  *
  * @author lenovo
  */
-public class PrescriptionModel {
-    private String time, description, medicine, dosage;
 
-    public Prescription(String time, String description, String medicine, String dosage) {
+public class PrescriptionModel {
+    private String time;
+    private String description;
+    private String medicine;
+    private String dosage;
+
+    public PrescriptionModel(String time, String description, String medicine, String dosage) {
         this.time = time;
         this.description = description;
         this.medicine = medicine;
         this.dosage = dosage;
     }
 
-    public String getTime() { return time; }
-    public String getDescription() { return description; }
-    public String getMedicine() { return medicine; }
-    public String getDosage() { return dosage; }
+    public String getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getMedicine() {
+        return medicine;
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setMedicine(String medicine) {
+        this.medicine = medicine;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
 }
+
