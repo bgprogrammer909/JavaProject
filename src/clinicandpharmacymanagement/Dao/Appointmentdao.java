d075231079ac649b5da8ed1d9f5138b659789c2f
sammysam578 <sambridhigyawali186@gmail.com>
2025-06-16 08:56:35 +0545

    appointmentdao done

--- src/clinicandpharmacymanagement/Dao/Appointmentdao.java
+++ src/clinicandpharmacymanagement/Dao/Appointmentdao.java
@@ -4,10 +4,37 @@
  */
 package clinicandpharmacymanagement.Dao;
 
+import clinicandpharmacymanagement.Database.MysqlConnection;
+import clinicandpharmacymanagement.view.model.AppointmentModel;
+import java.sql.*;
+
 /**
  *
  * @author user
  */
 public class Appointmentdao {
+    MysqlConnection mysql = new MysqlConnection();
+
+    public boolean insertAppointment(AppointmentModel appointmentModel) {
+        String query = "INSERT INTO appointments (id, appointment, patient, doctor) VALUES (?, ?, ?, ?)";
+        Connection conn = mysql.openConnection();
+
+        try {
+            PreparedStatement stmnt = conn.prepareStatement(query);
+            stmnt.setString(1, appointmentModel.getId());
+            stmnt.setString(2, appointmentModel.getAppointment());
+            stmnt.setString(3, appointmentModel.getPatient());
+            stmnt.setString(4, appointmentModel.getDoctor());
+
+            int result = stmnt.executeUpdate();
+            return result > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            mysql.closeConnection(conn);
+        }
+    }
+    
     
 }
