e162d8fe29dea5e1909395567e6b9cb2e6b9e8e7
sammysam578 <sambridhigyawali186@gmail.com>
2025-06-16 08:49:40 +0545

    billingdao changes for errors

--- src/clinicandpharmacymanagement/Dao/Billingsdao.java
+++ src/clinicandpharmacymanagement/Dao/Billingsdao.java
@@ -4,10 +4,37 @@
  */
 package clinicandpharmacymanagement.Dao;
 
+import clinicandpharmacymanagement.Database.MysqlConnection;
+import clinicandpharmacymanagement.view.model.BillingsModel;
+import java.sql.*;
+
 /**
  *
  * @author user
  */
 public class Billingsdao {
+    MysqlConnection mysql = new MysqlConnection();
+
+    public boolean insertMedicine(BillingsModel BillingsModel) {
+        String query = "INSERT INTO medicinemodel (id, medincename, unit, price, total) VALUES (?, ?, ?, ?, ?)";
+        java.sql.Connection conn=mysql.openConnection();
+
+        try {
+            PreparedStatement stmnt = conn.prepareStatement(query);
+            stmnt.setString(1, BillingsModel.getId());
+            stmnt.setString(2, BillingsModel.getMedicinename());
+            stmnt.setInt(3, BillingsModel.getUnit());
+            stmnt.setDouble(4, Double.parseDouble(BillingsModel.getPrice()));
+            stmnt.setDouble(5, BillingsModel.getUnit() * Double.parseDouble(BillingsModel.getPrice()));
+
+            int result = stmnt.executeUpdate();
+            return result > 0;
+        } catch (SQLException | NumberFormatException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            mysql.closeConnection(conn);
+        }
+    }
     
 }
--- src/clinicandpharmacymanagement/view/model/BillingsModel.java
+++ src/clinicandpharmacymanagement/view/model/BillingsModel.java
@@ -9,5 +9,60 @@ package clinicandpharmacymanagement.view.model;
  * @author user
  */
 public class BillingsModel {
+    private String id;
+    private String medicinename;
+    private int unit;
+    private String price;
+
+    // Constructor
+    public BillingsModel(String id, String medicinename, int unit, String price) {
+        this.id = id;
+        this.medicinename = medicinename;
+        this.unit = unit;
+        this.price = price;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getMedicinename() {
+        return medicinename;
+    }
+
+    public int getUnit() {
+        return unit;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    // Optional: Auto-calculated total
+    public double getTotal() {
+        try {
+            return unit * Double.parseDouble(price);
+        } catch (NumberFormatException e) {
+            return 0.0;
+        }
+    }
+
+    // Setters (if needed)
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setMedicinename(String medicinename) {
+        this.medicinename = medicinename;
+    }
+
+    public void setUnit(int unit) {
+        this.unit = unit;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
     
 }
