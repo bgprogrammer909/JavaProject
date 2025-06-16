0e46fb5439ea7b10ae71c52996d15349d8e485c6
Srizan-mhrz <shrijan.mhrz@gmail.com>
2025-06-16 09:00:03 +0545

    prescription controller

--- src/clinicandpharmacymanagement/Dao/PrescriptionsDao.java
+++ src/clinicandpharmacymanagement/Dao/PrescriptionsDao.java
@@ -9,5 +9,30 @@ package clinicandpharmacymanagement.Dao;
  * @author lenovo
  */
 public class PrescriptionsDao {
-    
+    Connection conn;
+
+    public PrescriptionDAO(Connection conn) {
+        this.conn = conn;
+    }
+
+    public List<Prescription> getAllPrescriptions() {
+        List<Prescription> list = new ArrayList<>();
+        String query = "SELECT * FROM prescriptions";
+        try {
+            PreparedStatement stmt = conn.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Prescription p = new Prescription(
+                    rs.getString("time"),
+                    rs.getString("description"),
+                    rs.getString("medicine"),
+                    rs.getString("dosage")
+                );
+                list.add(p);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
