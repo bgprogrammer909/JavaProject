dffbbe7f06998eeb45f26fa94f7ace551206935c
Srizan-mhrz <shrijan.mhrz@gmail.com>
2025-06-16 09:50:29 +0545

    made book appointmnet

--- src/clinicandpharmacymanagement/controller/PrescriptionController.java
+++ src/clinicandpharmacymanagement/controller/PrescriptionController.java
@@ -9,5 +9,27 @@ package clinicandpharmacymanagement.Controller;
  * @author lenovo
  */
 public class PrescriptionController {
-    
+    private PrescriptionView view;
+    private PrescriptionDAO dao;
+
+    public PrescriptionController(PrescriptionView view, PrescriptionDAO dao) {
+        this.view = view;
+        this.dao = dao;
+
+        this.view.getLoadButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loadPrescriptions();
+            }
+        });
+    }
+
+    private void loadPrescriptions() {
+        List<Prescription> list = dao.getAllPrescriptions();
+        DefaultTableModel model = view.getTableModel();
+        model.setRowCount(0); // clear old data
+        for (Prescription p : list) {
+            model.addRow(new Object[]{p.getTime(), p.getDescription(), p.getMedicine(), p.getDosage()});
+        }
+    }
 }
