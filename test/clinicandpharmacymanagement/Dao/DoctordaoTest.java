79cf82c94dd6e2ce305be77c335a284374a6e055
ankit21377 <ankitkharel678@gmail.com>
2025-06-20 17:44:55 +0545

    problem in doctor dao testing

--- test/clinicandpharmacymanagement/Dao/DoctordaoTest.java
+++ test/clinicandpharmacymanagement/Dao/DoctordaoTest.java
@@ -42,4 +42,15 @@ public class DoctordaoTest {
      * Test of addDoctor method, of class Doctordao.
      */
     @Test
-
+    public void testAddDoctor() {
+        System.out.println("addDoctor");
+        DoctorModel model = null;
+        Doctordao instance = new Doctordao();
+        boolean expResult = false;
+        boolean result = instance.addDoctor(model);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
