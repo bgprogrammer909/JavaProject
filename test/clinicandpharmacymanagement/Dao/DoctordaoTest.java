072c5fc4326ee382f6e0bfa6980ab016a2edcc39
ankit21377 <ankitkharel678@gmail.com>
2025-06-20 17:43:07 +0545

    doctor dao testing

---
+++ test/clinicandpharmacymanagement/Dao/DoctordaoTest.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package clinicandpharmacymanagement.Dao;
+
+import clinicandpharmacymanagement.view.model.DoctorModel;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author AngkitKharel
+ */
+public class DoctordaoTest {
+    
+    public DoctordaoTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+        System.out.println ("setup class");    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+         System.out.println("Setup before each class");
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addDoctor method, of class Doctordao.
+     */
+    @Test
+
