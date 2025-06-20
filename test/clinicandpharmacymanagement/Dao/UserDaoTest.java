4a170f11c1b83ba20b1b0572bc88cbb2694daf8c
ankit21377 <ankitkharel678@gmail.com>
2025-06-20 17:35:50 +0545

    testing userdao

---
+++ test/clinicandpharmacymanagement/Dao/UserDaoTest.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package clinicandpharmacymanagement.Dao;
+
+import clinicandpharmacymanagement.view.model.LoginRequest;
+import clinicandpharmacymanagement.view.model.ResetPasswordRequest;
+import clinicandpharmacymanagement.view.model.UserData;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author AngkitKharel
+ */
+public class UserDaoTest {
+    
+    public UserDaoTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of register method, of class UserDao.
+     */
+    @Test
+    public void testRegister() {
+        System.out.println("register");
+        UserData user = null;
+        UserDao instance = new UserDao();
+        boolean expResult = false;
+        boolean result = instance.register(user);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of login method, of class UserDao.
+     */
+    @Test
+    public void testLogin() {
+        System.out.println("login");
+        LoginRequest loginReq = null;
+        UserDao instance = new UserDao();
+        UserData expResult = null;
+        UserData result = instance.login(loginReq);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of checkEmail method, of class UserDao.
+     */
+    @Test
+    public void testCheckEmail() {
+        System.out.println("checkEmail");
+        String email = "";
+        UserDao instance = new UserDao();
+        boolean expResult = false;
+        boolean result = instance.checkEmail(email);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of resetPassword method, of class UserDao.
+     */
+    @Test
+    public void testResetPassword() {
+        System.out.println("resetPassword");
+        ResetPasswordRequest reset = null;
+        UserDao instance = new UserDao();
+        boolean expResult = false;
+        boolean result = instance.resetPassword(reset);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+ 
