eb59a9cc9a70fc0bb375ac6b4271c2d62b044904
SAKSHAMBAN219 <sakshamban@gmail.com>
2025-06-22 09:37:05 +0545

    back button insertion

--- src/clinicandpharmacymanagement/view/Paymentdash.java
+++ src/clinicandpharmacymanagement/view/Paymentdash.java
@@ -186,7 +186,8 @@ public class Paymentdash extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-  
+      this.dispose(); // close current Paymentdash window
+    new MainDashboard().setVisible(true);
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
