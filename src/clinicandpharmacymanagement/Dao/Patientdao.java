3d770aa7d2be9c7b012dffdd99eb5003b4ab973d
ankit21377 <ankitkharel678@gmail.com>
2025-06-22 09:21:53 +0545

    update

--- src/clinicandpharmacymanagement/Dao/Patientdao.java
+++ src/clinicandpharmacymanagement/Dao/Patientdao.java
@@ -1,40 +1,40 @@
-///*
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-// */
-//package clinicandpharmacymanagement.Dao;
-//
-//import clinicandpharmacymanagement.Database.MysqlConnection;
-//import clinicandpharmacymanagement.view.model.PatientModel;
-//import java.sql.*;
-///**
-// *
-// * @author AngkitKharel
-// */
-//public class Patientdao {
-//    MysqlConnection mysql=new MysqlConnection();
-//    public boolean addPatient(PatientModel Model){
-//    String query ="insert into Patient(id,name,address,phone,Condition,emergency) values(?,?,?,?,?,?)";
-//        Connection conn=mysql.openConnection();
-//        try{
-//            
-//            PreparedStatement stmnt=conn.prepareStatement(query);
-//            stmnt.setString(1,Model.getId());
-//            stmnt.setString(2,Model.getName());
-//            stmnt.setString(3, Model.getAddress());
-//            stmnt.setString(4,Model.getPhone());
-//            stmnt.setString(5,Model.getCondition());
-//            stmnt.setString(6,Model.getEmergency());
-//           
-//            
-//            int result=stmnt.executeUpdate();
-//            return result>0;
-//        } catch(SQLException e){
-//            return false;
-//        }finally {
-//            mysql.closeConnection(conn);
-//        }
-//    }
-//    
-//}
-//
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clinicandpharmacymanagement.Dao;
+
+import clinicandpharmacymanagement.Database.MysqlConnection;
+import clinicandpharmacymanagement.view.model.PatientModel;
+import java.sql.*;
+
+
+/**
+ *
+ * @author user
+ */
+public class PatientDao {
+    MysqlConnection mysql = new MysqlConnection();
+
+    public boolean insertPatient(PatientModel patientModel) {
+        String query = "INSERT INTO patients (patient_id, name, doctor, time) VALUES (?, ?, ?, ?)";
+        Connection conn = mysql.openConnection();
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, patientModel.getPatientId());
+            stmt.setString(2, patientModel.getName());
+            stmt.setString(3, patientModel.getDoctor());
+            stmt.setString(4, patientModel.getTime());
+
+            int result = stmt.executeUpdate();
+            return result > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            mysql.closeConnection(conn);
+        }
+    }
+    
+}
